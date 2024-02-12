3e5da270010891ef9a278d55285db8897d127adf
Jian Weihang <tonytonyjan@gmail.com>
2024-02-13 00:17:53 +0800

    refactor: improve RandomGaussian's performance

--- lib/random_gaussian.rb
+++ lib/random_gaussian.rb
@@ -6,12 +6,18 @@ class RandomGaussian
     @random_number_generator = random_number_generator
     @mean = mean
     @stddev = stddev
-    @samples = []
+    @valid = false
+    @next = nil
   end
 
   def rand
-    sample! if @samples.empty?
-    @samples.pop
+    if @valid
+      @valid = false
+      @next
+    else
+      @valid = true
+      sample!
+    end
   end
 
   private
@@ -20,7 +26,7 @@ class RandomGaussian
     theta = 2 * Math::PI * @random_number_generator.rand
     radius = Math.sqrt(-2 * Math.log(1 - @random_number_generator.rand))
     scale = @stddev * radius
-    @samples << @mean + scale * Math.cos(theta)
-    @samples << @mean + scale * Math.sin(theta)
+    @next = @mean + scale * Math.sin(theta)
+    @mean + scale * Math.cos(theta)
   end
 end
