671ed794fe04daffd1975af26a53c725adb254e6
Jian Weihang <tonytonyjan@gmail.com>
2024-02-13 23:24:44 +0800

    fix: decouple global constant

--- lib/active_support_cache_store_extension.rb
+++ lib/active_support_cache_store_extension.rb
@@ -2,7 +2,7 @@
 
 module ActiveSupportCacheStoreExtension
   def x_fetch(key, expires_in:, **)
-    entry = CACHE.send(:read_entry, key)
+    entry = read_entry(key)
 
     if !entry || (Time.now.to_f - entry.instance_variable_get(:@delta) * Math.log(rand)) >= entry.expires_at
       start = Time.now.to_f
@@ -10,7 +10,7 @@ module ActiveSupportCacheStoreExtension
       delta = Time.now.to_f - start
       entry = ActiveSupport::Cache::Entry.new(value, expires_in:)
       entry.instance_variable_set(:@delta, delta)
-      CACHE.send(:write_entry, key, entry, expires_in:)
+      write_entry(key, entry, expires_in:)
     end
     entry.value
   end
